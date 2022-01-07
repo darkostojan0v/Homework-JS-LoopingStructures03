fb281e89eedb2baa400e9424cdeeeaad5146ab27
darkostojan0v <95142705+darkostojan0v@users.noreply.github.com>
2022-01-07 17:02:59 +0100

    Add files via upload

---
+++ HomeworkJS Looping Structures 3/script.js
@@ -0,0 +1,7 @@
+console.log('Looping Structures');
+
+    const firstName = ["Bojan", "Nikola", "Jana"];
+    const lastName = ["Stefanovski", "Hristovska", "Kostovski"];
+    const fullName = [`1.${firstName[0]} ${lastName[2]}, 2.${firstName[1]} ${lastName[0]}, 3.${firstName[2]} ${lastName[1]}`];
+
+    console.log(fullName);
